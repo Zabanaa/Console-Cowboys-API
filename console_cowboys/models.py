41a874265a0a7165f9a18cc4473185fef6111731
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-07-16 12:37:30 +0000

    handle database errors

--- console_cowboys/models.py
+++ console_cowboys/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+from console_cowboys.helpers import ErrorResponse
 from console_cowboys import db
 from sqlalchemy_utils import ChoiceType
+from sqlalchemy.exc import IntegrityError
 
 class Job(db.Model):
 
@@ -15,7 +17,7 @@ class Job(db.Model):
     location            = db.Column(db.String(90), nullable=False)
     contract_type       = db.Column(ChoiceType(CONTRACT_TYPES), nullable=False)
     company_name        = db.Column(db.String(90), nullable=False)
-    listing_url         = db.Column(db.String(90), nullable=False)
+    listing_url         = db.Column(db.String(90), nullable=False, unique=True)
     is_remote           = db.Column(db.Boolean, default=False)
     is_paid             = db.Column(db.Boolean, default=False)
     date_added          = db.Column(db.DateTime, default=datetime.utcnow)
@@ -45,8 +47,20 @@ class Job(db.Model):
         try:
             db.session.add(self)
             db.session.commit()
-        except Exception as e:
-            print(e)
+
+        except IntegrityError as e:
+
+            cause_of_error = str(e.__dict__["orig"])
+
+            if "not-null" in cause_of_error:
+                missing_fields = e.__dict__["params"]
+                return ErrorResponse.missing_fields_error(missing_fields)
+
+            elif "unique" in cause_of_error:
+                print(ErrorResponse.unique_field_error(cause_of_error))
+
+            else:
+                print("Something Happened")
 
         return self
 
